688066d9a150d7cbebd0a85a99186f825ca969e3
Thomas Reitelbach <tr@erdfunkstelle.de>
2007-08-03 09:04:40 +0000

    catch all messages
    svn path=/trunk/playground/devtools/kaider/; revision=695902

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,13 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` > rc.cpp
-$XGETTEXT *.cpp -o $podir/kaider.pot
+$XGETTEXT *.cpp \
+	    catalog/*.cpp \
+	    cataloglistview/*.cpp \
+	    common/*.cpp \
+	    glossary/*.cpp \
+	    mergemode/*.cpp \
+	    prefs/*.cpp \
+	    project/*.cpp \
+	    tm/*.cpp \
+	    webquery/*.cpp \
+	    -o $podir/kaider.pot
