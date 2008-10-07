75d4cad0d173c9d7175a91db1526eff679a346d5
Nick Shaforostoff <shafff@ukr.net>
2008-10-07 00:59:56 +0000

    launch lokalize instance if it isn't opened yet
    svn path=/trunk/KDE/kdesdk/lokalize/; revision=868748

--- src/scripts/find-gui-text.sh
+++ src/scripts/find-gui-text.sh
@@ -1,7 +1,20 @@
 TEXT=$1
 PACKAGE=$2
+FOUND=0
 LOKALIZE_INSTANCES=`qdbus org.kde.lokalize*`
-for LOKALIZE_INSTANCE in $LOKALIZE_INSTANCES; do \
-qdbus $LOKALIZE_INSTANCE /ThisIsWhatYouWant org.kde.Lokalize.MainWindow.showTranslationMemory; \
-qdbus $LOKALIZE_INSTANCE /ThisIsWhatYouWant/TranslationMemory/0 org.kde.Lokalize.TranslationMemory.findGuiTextPackage "$TEXT" "$PACKAGE"; \
+if [ -z $LOKALIZE_INSTANCES ]; then lokalize>/dev/null 2>/dev/null & fi
+
+let COUNTER=0
+while [ -z $LOKALIZE_INSTANCES ]; do \
+let COUNTER=$COUNTER+1
+if [ $COUNTER -gt 10 ]; then exit ; fi
+sleep 1
+LOKALIZE_INSTANCES=`qdbus org.kde.lokalize*` ;\
 done
+
+for LOKALIZE_INSTANCE in $LOKALIZE_INSTANCES; do \
+    FOUND=1
+    qdbus $LOKALIZE_INSTANCE /ThisIsWhatYouWant org.kde.Lokalize.MainWindow.showTranslationMemory; \
+    qdbus $LOKALIZE_INSTANCE /ThisIsWhatYouWant/TranslationMemory/0 org.kde.Lokalize.TranslationMemory.findGuiTextPackage "$TEXT" "$PACKAGE"; \
+done
+
