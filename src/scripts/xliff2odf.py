3c901059f1fd01a768baf67c6226a9b4d840d7df
Nick Shaforostoff <shafff@ukr.net>
2009-02-05 22:55:39 +0000

    sorry, forgot this ;) SVN_SILENT
    svn path=/trunk/KDE/kdesdk/lokalize/; revision=921921

--- src/scripts/xliff2odf.py
+++ src/scripts/xliff2odf.py
@@ -6,14 +6,25 @@ import sys,os
 from translate.convert import xliff2odf
 
 def convert():
-    if not Lokalize.activeEditor() or Lokalize.activeEditor().currentFile()=='': return
+    def args(src, tgt, **kwargs):
+        arg_list = [u'--errorlevel=traceback', src, tgt]
+        for flag, value in kwargs.iteritems():
+            value = unicode(value)
+            if len(flag) == 1:
+                arg_list.append(u'-%s' % flag)
+            else:
+                arg_list.append(u'--%s' % flag)
+            if value is not None:
+                arg_list.append(value)
+        return arg_list
 
+    if not Lokalize.activeEditor() or Lokalize.activeEditor().currentFile()=='': return
     
     xliffpathname=Lokalize.activeEditor().currentFile()
-    (path, filename)=xliffpathname
+    (path, filename)=os.path.split(xliffpathname)
     if not filename.endswith('.xlf'): return
-    odfpathname=os.path.splitext(filename)[0]+'.odt'
-    translatedodfpathname=os.path.splitext(filename)[0]+'-'+Project.targetLangCode()+'.odt'
+    odfpathname=os.path.splitext(xliffpathname)[0]+'.odt'
+    translatedodfpathname=os.path.splitext(xliffpathname)[0]+'-'+Project.targetLangCode()+'.odt'
 
     print xliffpathname
     print odfpathname
