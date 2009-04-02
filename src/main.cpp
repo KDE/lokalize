9575135c0f5007c1df102e0deb58ede81e30f1e4
Nick Shaforostoff <shafff@ukr.net>
2009-04-02 20:05:40 +0000

    gosh, i almost forgot this: add Viesturs Zarins to credits section!
    svn path=/trunk/KDE/kdesdk/lokalize/; revision=948346

--- src/main.cpp
+++ src/main.cpp
@@ -57,6 +57,7 @@ int main(int argc, char **argv)
     about.addAuthor( ki18n("Nick Shaforostoff"), KLocalizedString(), "shaforostoff@kde.ru" );
     about.addCredit (ki18n("Google Inc."), ki18n("sponsored development as part of Google Summer Of Code program"), QByteArray(), "http://google.com");
     about.addCredit (ki18n("Translate-toolkit"), ki18n("provided excellent cross-format converting scripts"), QByteArray(), "http://translate.sourceforge.net");
+    about.addCredit (ki18n("Viesturs Zarins"), ki18n("project tree merging translation+templates"), "viesturs.zarins@mii.lu.lv", QByteArray());
     about.addCredit (ki18n("Stephan Johach"), ki18n("bug fixing patches"), "hunsum@gmx.de");
     about.addCredit (ki18n("Chusslove Illich"), ki18n("bug fixing patches"), "caslav.ilic@gmx.net");
     KCmdLineArgs::init(argc, argv, &about);
