fec7735f8f1c67bcfa041d7d67662556f6aa70a9
Albert Astals Cid <aacid@kde.org>
2009-05-12 19:58:46 +0000

    Fix Messages.sh once scripts dir has moved CCMAIL: shafff@ukr.net
    svn path=/trunk/KDE/kdesdk/lokalize/; revision=967183

--- Messages.sh
+++ Messages.sh
@@ -1,16 +1,16 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
 $EXTRACTATTR --attr=collection,text --attr=collection,comment --attr=script,text --attr=script,comment scripts/*.rc >> rc.cpp || exit 11
-$XGETTEXT *.cpp \
-	    catalog/*.cpp \
-	    cataloglistview/*.cpp \
-	    common/*.cpp \
-	    glossary/*.cpp \
-	    mergemode/*.cpp \
-	    prefs/*.cpp \
-	    project/*.cpp \
-	    tm/*.cpp \
-	    webquery/*.cpp \
+$XGETTEXT src/*.cpp \
+	    src/catalog/*.cpp \
+	    src/cataloglistview/*.cpp \
+	    src/common/*.cpp \
+	    src/glossary/*.cpp \
+	    src/mergemode/*.cpp \
+	    src/prefs/*.cpp \
+	    src/project/*.cpp \
+	    src/tm/*.cpp \
+	    src/webquery/*.cpp \
 	    -o $podir/lokalize.pot
 rm -f rc.cpp
 $XGETTEXT --language=Python --join-existing scripts/*.py -o $podir/lokalize.pot
