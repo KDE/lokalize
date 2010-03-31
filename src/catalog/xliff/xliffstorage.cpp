12a76cef75785e6c4bd3297a5aa606fa365f64a1
Laszlo Papp <lpapp@kde.org>
2010-03-31 04:56:00 +0000

    Remove trailing whitespaces from xliffstorage.cpp
    svn path=/trunk/KDE/kdesdk/lokalize/; revision=1109343

--- src/catalog/xliff/xliffstorage.cpp
+++ src/catalog/xliff/xliffstorage.cpp
@@ -6,7 +6,7 @@ modify it under the terms of the GNU General Public License as
 published by the Free Software Foundation; either version 2 of
 the License or (at your option) version 3 or any later version
 accepted by the membership of KDE e.V. (or its successor approved
-by the membership of KDE e.V.), which shall act as a proxy 
+by the membership of KDE e.V.), which shall act as a proxy
 defined in Section 14 of version 3 of the license.
 
 This program is distributed in the hope that it will be useful,
@@ -136,10 +136,10 @@ int XliffStorage::load(QIODevice* device)
 //             while (msgstr.count()<m_numberOfPluralForms)
 //                 msgstr.append(QString());
 //             item.setMsgstr(msgstr);
-// 
+//
 //         }
 //         ++i;
-// 
+//
 //     }
 
 
