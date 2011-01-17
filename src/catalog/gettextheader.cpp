9eab258d32f0d0a8f4fa99f3d9017e1e388db878
Nick Shaforostoff <shafff@ukr.net>
2011-01-17 18:41:05 +0000

    backport hungarian plural form expression change CCBUG:263342
    svn path=/branches/KDE/4.6/kdesdk/lokalize/; revision=1215128

--- src/catalog/gettextheader.cpp
+++ src/catalog/gettextheader.cpp
@@ -107,7 +107,7 @@ static const char* pInfo[NUM_LANG_WITH_INFO]={
 "nplurals=3; plural=n==1 ? 0 : n==2 ? 1 : 2;",
 "nplurals=2; plural=(n != 1);",
 "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);",
-"nplurals=1; plural=0;",
+"nplurals=2; plural=(n != 1);",
 "nplurals=2; plural=(n != 1);",
 "nplurals=1; plural=0;",
 "nplurals=1; plural=0;",
