bd57b12e5b63d7b2e10320487bc65d060a77c01d
Burkhard Lück <lueck@hube-lueck.de>
2013-12-15 14:39:00 +0100

    use find for extraction, more robust wrt code file moves BUG:328806

--- Messages.sh
+++ Messages.sh
@@ -1,17 +1,6 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
 $EXTRACTATTR --attr=collection,text --attr=collection,comment --attr=script,text --attr=script,comment scripts/*.rc >> rc.cpp || exit 11
-$XGETTEXT rc.cpp src/*.cpp \
-	    src/catalog/*.cpp \
-	    src/cataloglistview/*.cpp \
-	    src/common/*.cpp \
-	    src/glossary/*.cpp \
-	    src/mergemode/*.cpp \
-	    src/prefs/*.cpp \
-	    src/project/*.cpp \
-	    src/tm/*.cpp \
-	    src/webquery/*.cpp \
-            src/filesearch/*cpp \
-	    -o $podir/lokalize.pot
+$XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/lokalize.pot
 rm -f rc.cpp
 $XGETTEXT --language=Python --join-existing scripts/*.py -o $podir/lokalize.pot
