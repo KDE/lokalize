ad2e296cb5bcc4a12d1294fededb877877ce531d
Nick Shaforostoff <shafff@ukr.net>
2015-10-21 23:33:16 +0300

    BUG: 348086 fix crashes on the systems that are so fast that can process 1 file in less than 1 millisecond

--- src/tm/tmscanapi.cpp
+++ src/tm/tmscanapi.cpp
@@ -71,7 +71,7 @@ void RecursiveScanJob::scanJobFinished(ThreadWeaver::Job* j)
     emitPercent(processedAmount(KJob::Files),totalAmount(KJob::Files));
 
     setProcessedAmount(KJob::Bytes,processedAmount(KJob::Bytes)+job->m_size);
-    emitSpeed(1000*processedAmount(KJob::Bytes)/m_time.elapsed());
+    if (m_time.elapsed()) emitSpeed(1000*processedAmount(KJob::Bytes)/m_time.elapsed());
 
 
     if (processedAmount(KJob::Files)==totalAmount(KJob::Files))
