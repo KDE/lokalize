6be879c8f9e33c22b5a7c91333dc6e27595baf01
Simon Depiets <sdepiets@gmail.com>
2018-09-03 14:33:27 +0800

    Removing non useful debug info

--- src/catalog/catalogstring.cpp
+++ src/catalog/catalogstring.cpp
@@ -266,7 +266,7 @@ QDataStream &operator>>(QDataStream &in, CatalogString &myObj)
 
 void adaptCatalogString(CatalogString& target, const CatalogString& ref)
 {
-    qCWarning(LOKALIZE_LOG) << "HERE" << target.string;
+    //qCWarning(LOKALIZE_LOG) << "HERE" << target.string;
     QHash<QString, int> id2tagIndex;
     QMultiMap<InlineTag::InlineElement, int> tagType2tagIndex;
     int i = ref.tags.size();
@@ -291,7 +291,7 @@ void adaptCatalogString(CatalogString& target, const CatalogString& ref)
             oldTags.removeAt(i);
         }
     }
-    qCWarning(LOKALIZE_LOG) << "HERE 0" << target.string;
+    //qCWarning(LOKALIZE_LOG) << "HERE 0" << target.string;
 
     //now all the tags left have to ID (exact) matches
     i = oldTags.size();
@@ -313,13 +313,13 @@ void adaptCatalogString(CatalogString& target, const CatalogString& ref)
             oldTags.removeAt(i);
         }
     }
-    qCWarning(LOKALIZE_LOG) << "HERE 1" << target.string;
+    //qCWarning(LOKALIZE_LOG) << "HERE 1" << target.string;
     //now walk through unmatched tags and properly remove them.
     foreach (const InlineTag& tag, oldTags) {
         if (tag.isPaired())
             target.remove(tag.end, 1);
         target.remove(tag.start, 1);
     }
-    qCWarning(LOKALIZE_LOG) << "HERE 2" << target.string;
+    //qCWarning(LOKALIZE_LOG) << "HERE 2" << target.string;
 }
 
