72c0c5c95095b8fd3f2c5e7e924cb94ef16e31c4
Albert Astals Cid <aacid@kde.org>
2019-03-27 19:31:08 +0100

    Use new hunspell API
    Test Plan: I have only checked it compiles, have not actually run the feature to make sure it still works
    
    Reviewers: sdepiets
    
    Reviewed By: sdepiets
    
    Subscribers: yurchor, #localization
    
    Differential Revision: https://phabricator.kde.org/D20071

--- src/common/stemming.cpp
+++ src/common/stemming.cpp
@@ -98,16 +98,11 @@ QString stem(const QString& langCode, const QString& word)
     if (!speller)
         return word;
 
-    char** result1;
-    char** result2;
-    int n1 = speller->analyze(&result1, sc.codec->fromUnicode(word));
-    int n2 = speller->stem(&result2, result1, n1);
+    const std::vector<std::string> result1 = speller->analyze(sc.codec->fromUnicode(word).toStdString());
+    const std::vector<std::string> result2 = speller->stem(result1);
 
-    if (n2)
-        result = sc.codec->toUnicode(result2[0]);
-
-    speller->free_list(&result1, n1);
-    speller->free_list(&result2, n2);
+    if (!result2.empty())
+        result = sc.codec->toUnicode(QByteArray::fromStdString(result2[0]));
 #endif
 
     return result;
