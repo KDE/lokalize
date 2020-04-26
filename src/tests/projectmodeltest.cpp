3ad463576dc1b609f1142b43e4974f82b72fc4ab
Simon Depiets <sdepiets@gmail.com>
2020-04-26 18:13:00 +0800

    Cleanup warnings

--- src/tests/projectmodeltest.cpp
+++ src/tests/projectmodeltest.cpp
@@ -87,7 +87,7 @@ void ProjectModelTest::testHalfTranslated()
     model->setUrl(QUrl::fromLocalFile(QFINDTESTDATA("data/dir-halftranslated")), {});
 
     // Wait for signal
-    while (!loaded.load()) {
+    while (!loaded.loadRelaxed()) {
         QCoreApplication::processEvents();
     }
 
