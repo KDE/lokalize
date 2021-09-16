7337d6c234e3b5e1f75fc8f35a61a77ee25061c3
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-09-16 21:09:25 +0200

    Convert Qt-Commercial-exception statements to SPDX

--- src/webquery/myactioncollectionview.h
+++ src/webquery/myactioncollectionview.h
@@ -4,19 +4,7 @@
   SPDX-FileCopyrightText: 2007 Nick Shaforostoff <shafff@ukr.net>
   SPDX-FileCopyrightText: 2018-2019 Simon Depiets <sdepiets@gmail.com>
 
-  SPDX-License-Identifier: GPL-2.0-or-later
-
-  In addition, as a special exception, the copyright holders give
-  permission to link the code of this program with any edition of
-  the Qt library by Trolltech AS, Norway (or with modified versions
-  of Qt that use the same license as Qt), and distribute linked
-  combinations including the two.  You must obey the GNU General
-  Public License in all respects for all of the code used other than
-  Qt. If you modify this file, you may extend this exception to
-  your version of the file, but you are not obligated to do so.  If
-  you do not wish to do so, delete this exception statement from
-  your version.
-
+  SPDX-License-Identifier: GPL-2.0-or-later WITH LicenseRef-Qt-Commercial-exception-1.0
 */
 
 #ifndef MYACTIONCOLLECTIONVIEW_H
