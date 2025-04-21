KDE/lokalize

commit 45be20008d181682114db201545f27e4f7854b5c

Port Project Overview from QMainWindow
Remove Project Overview tab QMainWindow parent class. Relies
on https://invent.kde.org/sdk/lokalize/-/merge_requests/222
which duplicates the tab page base class, without QMainWindow.

src/lokalizemainwindow.cpp
@@ -235,7 +235,12 @@ void LokalizeMainWindow::activateTabAtIndex(int i)
     // This disconnects the old keyboard shortcuts and connects those
     // related to the currently visible tab.
     guiFactory()->removeClient(m_activeTabPageKeyboardShortcuts);
-    m_activeTabPageKeyboardShortcuts = static_cast<LokalizeTabPageBase *>(m_mainTabs->currentWidget())->guiClient();
+    QWidget *currentTab = m_mainTabs->currentWidget();
+    if (LokalizeTabPageBase *activeTab = qobject_cast<LokalizeTabPageBase *>(currentTab)) {
+        m_activeTabPageKeyboardShortcuts = activeTab->guiClient();
+    } else if (LokalizeTabPageBaseNoQMainWindow *activeTab = qobject_cast<LokalizeTabPageBaseNoQMainWindow *>(currentTab)) {
+        m_activeTabPageKeyboardShortcuts = activeTab->guiClient();
+    }
     guiFactory()->addClient(m_activeTabPageKeyboardShortcuts);
 }
 

src/project/projecttab.cpp
@@ -43,15 +43,14 @@
 #include <QVBoxLayout>
 
 ProjectTab::ProjectTab(QWidget *parent)
-    : LokalizeTabPageBase(parent)
+    : LokalizeTabPageBaseNoQMainWindow(parent)
     , m_browser(new ProjectWidget(this))
     , m_filterEdit(new QLineEdit(this))
 
 {
     m_tabLabel = i18nc("@title:tab", "Project Overview");
     m_tabIcon = QIcon::fromTheme(QStringLiteral("project-open"));
-    QWidget *w = new QWidget(this);
-    QVBoxLayout *l = new QVBoxLayout(w);
+    QVBoxLayout *l = new QVBoxLayout(this);
 
     m_filterEdit->setClearButtonEnabled(true);
     m_filterEdit->setPlaceholderText(i18n("Quick search..."));
@@ -66,7 +65,6 @@ ProjectTab::ProjectTab(QWidget *parent)
     connect(Project::instance()->model(), &ProjectModel::totalsChanged, this, &ProjectTab::updateStatusBar);
     connect(Project::instance()->model(), &ProjectModel::loadingAboutToStart, this, &ProjectTab::initStatusBarProgress);
 
-    setCentralWidget(w);
     QStatusBar *statusBar = static_cast<LokalizeTabPageBase *>(parent)->statusBar();
 
     m_progressBar = new QProgressBar(nullptr);

src/project/projecttab.h
@@ -27,7 +27,7 @@ class QProgressBar;
 /**
  * Project Overview Tab
  */
-class ProjectTab : public LokalizeTabPageBase
+class ProjectTab : public LokalizeTabPageBaseNoQMainWindow
 {
     Q_OBJECT
     Q_CLASSINFO("D-Bus Interface", "org.kde.Lokalize.ProjectOverview")

commit d9d98f1ca28f0bf030390a6bd6e730e0a844ed1a

Fix crash introduced by 45be20008d181682114db201545f27e4f7854b5c We were missing some parallel code to handle the new tab base class.

src/lokalizemainwindow.cpp
@@ -1007,7 +1007,12 @@ void LokalizeMainWindow::closeTabAtIndex(int index)
         return;
     }
     // This disconnects the keyboard shortcuts relating to the tab being closed.
-    guiFactory()->removeClient(static_cast<LokalizeTabPageBase *>(m_mainTabs->widget(index)));
+    QWidget *currentTab = m_mainTabs->currentWidget();
+    if (LokalizeTabPageBase *activeTab = qobject_cast<LokalizeTabPageBase *>(currentTab)) {
+        guiFactory()->removeClient(activeTab);
+    } else if (LokalizeTabPageBaseNoQMainWindow *activeTab = qobject_cast<LokalizeTabPageBaseNoQMainWindow *>(currentTab)) {
+        guiFactory()->removeClient(activeTab);
+    }
     if (m_mainTabs->currentIndex() == index) {
         m_activeTabPageKeyboardShortcuts = nullptr;
     }