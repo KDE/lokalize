1ede60129d18cee966c6bc3b04b3a7b1346d0200
Albert Astals Cid <aacid@kde.org>
2025-04-22 23:03:38 +0200

    Fix Messages.sh file
    There's no rc nor py script files anymore

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
-$EXTRACTATTR --attr=collection,text --attr=collection,comment --attr=script,text --attr=script,comment scripts/*.rc >> rc.cpp || exit 11
 $XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/lokalize.pot
 rm -f rc.cpp
-$XGETTEXT --language=Python --join-existing scripts/*.py -o $podir/lokalize.pot
