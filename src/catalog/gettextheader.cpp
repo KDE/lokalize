765e8fb13743034378e6dd28350def92558c07e9
Johnny Jazeix <jazeix@gmail.com>
2025-07-07 05:59:28 +0000

    fix ar plural form
    See https://mail.kde.org/pipermail/kde-i18n-doc/2025-July/002900.html

--- src/catalog/gettextheader.cpp
+++ src/catalog/gettextheader.cpp
@@ -39,7 +39,7 @@ struct langPInfo {
 };
 
 static const langPInfo langsWithPInfo[] = {
-    {"ar", "nplurals=6; plural=n==0 ? 0 : n==1 ? 1 : n==2 ? 2 : n%100>=3 && n%100<=10 ? 3 : n%100>=11 && n%100<=99 ? 4 : 5;"},
+    {"ar", "nplurals=6; plural=n==0 ? 0 : n==1 ? 1 : n==2 ? 2 : n%100>=3 && n%100<=10 ? 3 : n%100>=11 ? 4 : 5;"},
     {"be@latin", "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);"},
     {"be", "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);"},
     {"bg", "nplurals=2; plural=(n != 1);"},
