84493c973aff202271a5e16b34311c8498e931e3
Jaimukund Bhan <bhanjaimukund@gmail.com>
2026-04-13 08:47:45 +0530

    Remove comment

--- src/project/projecttab.h
+++ src/project/projecttab.h
@@ -81,9 +81,6 @@ private Q_SLOTS:
     void scanFilesToTM();
     void pologyOnFiles();
     void addComment();
-    /**
-     * A sole purpose of this slot is to workaround the bug #460634.
-     */
     void searchInFiles(bool templ = false);
     void searchInFilesInclTempl();
     void openFile();
